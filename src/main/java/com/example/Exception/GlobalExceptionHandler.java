604eaf4f38b95595813fb745a9619137b84df107
Mat33nAhmad <mat33nahmad@gmail.com>
2023-01-12 18:43:55 +0500

    Renamed bad named folder

--- src/main/java/com/example/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/Exception/GlobalExceptionHandler.java
@@ -1,10 +1,7 @@
-package com.example.exception;
+package com.example.Exception;
 
-import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
